d46fd01c833778824f8b789669b22c95d8df0724
Jhanmaicol Jesus <jhanmaicol_jesus@hotmail.com>
2017-05-30 21:02:09 -0500

    Primer vol.

---
+++ RestauranteConHerenciaMVC/RestauranteStore.MVC/Controllers/AlmacenController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestauranteStore.Entities.Entities;
+using RestauranteStore.Persistence;
+//using RestauranteStore.Persistence.Repositories;
+using RestauranteStore.Entities.IRepositories;
+
+namespace RestauranteStore.MVC.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        // private RestauranteStoreDbContext db = new RestauranteStoreDbContext();
+        //private UnityOfWork unityOfWork = UnityOfWork.Instance;
+        private readonly IUnityOfWork _UnityOfWork;
+
+        public AlmacenController(IUnityOfWork unityOfWork)
+        {
+            _UnityOfWork = unityOfWork;
+        }
+
+        public AlmacenController()
+        {
+
+        }
+
+        // GET: Almacens
+        public ActionResult Index()
+        {
+            //return View(db.Almacenes.ToList());
+            return View(_UnityOfWork.Almacenes.GetAll());
+        }
+
+        // GET: Almacens/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Almacen almacen = db.Almacenes.Find(id);
+            Almacen almacen = _UnityOfWork.Almacenes.Get(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Almacens/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlmacenId,Inventario,EstadoAlm")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Almacenes.Add(almacen);
+                _UnityOfWork.Almacenes.Add(almacen);
+                //db.SaveChanges();
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(almacen);
+        }
+
+        // GET: Almacens/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Almacen almacen = db.Almacenes.Find(id);
+            Almacen almacen = _UnityOfWork.Almacenes.Get(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacens/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AlmacenId,Inventario,EstadoAlm")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(almacen).State = EntityState.Modified;
+                _UnityOfWork.StateModedified(almacen);
+                //db.SaveChanges();
+                _UnityOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacens/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Almacen almacen = db.Almacenes.Find(id);
+            Almacen almacen = _UnityOfWork.Almacenes.Get(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //Almacen almacen = db.Almacenes.Find(id);
+            Almacen almacen = _UnityOfWork.Almacenes.Get(id);
+
+            //db.Almacenes.Remove(almacen);
+            _UnityOfWork.Almacenes.Delete(almacen);
+
+            //db.SaveChanges();
+            _UnityOfWork.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+                _UnityOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RestauranteConHerenciaMVC/RestauranteStore.Persistence/Repositories/UnityOfWork.cs
@@ -0,0 +1,97 @@
+﻿using RestauranteStore.Entities.IRepositories;
+using RestauranteStore.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using RestauranteStore.Entities.Entities;
+
+namespace RestauranteStore.Persistence.Repositories
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private readonly RestauranteStoreDbContext _Context;
+        //private static UnityOfWork _Instance;
+        //private static readonly object _Lock = new object();
+
+        public IAlmacenRepository Almacenes { get; private set; }
+        public ICancelarReservaRepository CancelarReservas { get; private set; }
+        public ICartaRepository Cartas { get; private set; }
+        public IClienteRepository Clientes { get; private set; }
+        public IEmpleadoRepository Empleados { get; private set; }
+        public IEspecialidadDiaRepository EspecialidadDias { get; private set; }
+        public IEstadoPedidoRepository EstadoPedidos { get; private set; }
+        public IIngredienteRepository Ingredientes { get; private set; }
+        public IMesaRepository Mesas { get; private set; }
+        public IPedidoRepository Pedidos { get; private set; }
+        public IPersonaRepository Personas { get; private set; }
+        public IPlatoComidaRepository PlatoComidas { get; private set; }
+        public IProveedorRepository Proveedors { get; private set; }
+        public IReservaRepository Reservas { get; private set; }
+        public ITipoEmpleadoRepository TipoEmpleados { get; private set; }
+        public ITurnoRepository Turnos { get; private set; }
+
+        public UnityOfWork()
+        {
+
+        }
+
+        private UnityOfWork(RestauranteStoreDbContext context)
+        {
+            //_Context = new RestauranteStoreDbContext();
+            _Context = context;
+
+            Almacenes = new AlmacenRepository(_Context);
+            CancelarReservas = new CancelarReservaRepository(_Context);
+            Cartas = new CartaRepository(_Context);
+            EspecialidadDias = new EspecialidadDiaRepository(_Context);
+            EstadoPedidos = new EstadoPedidoRepository(_Context);
+            Ingredientes = new IngredienteRepository(_Context);
+            Mesas = new MesaRepository(_Context);
+            Pedidos = new PedidoRepository(_Context);
+            Personas = new PersonaRepository(_Context);
+            PlatoComidas = new PlatoComidaRepository(_Context);
+            Reservas = new ReservaRepository(_Context);
+            TipoEmpleados = new TipoEmpleadoRepository(_Context);
+            Turnos = new TurnoRepository(_Context);
+
+
+        }
+
+        public static UnityOfWork Instance
+        {
+            get
+            {
+                lock (_Lock)
+                {
+                    if (_Instance == null)
+                        _Instance = new UnityOfWork();
+                }
+
+                return _Instance;
+
+            }
+        }
+
+        //public void Dispose()
+        //{
+        //    _Context.Dispose();
+        //}
+
+        //public int SaveChanges()
+        //{
+        //    return _Context.SaveChanges();
+        //}
+
+        //public void StateModedified(object Entity)
+        //{
+        //    _Context.Entry(Entity).State = System.Data.Entity.EntityState.Modified;
+        //}
+
+        //public void StateModedified(Almacen almacen)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
